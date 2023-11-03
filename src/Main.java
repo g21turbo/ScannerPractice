18fa2fe503fa97b4958a3415989007d52a82ae6a
g21turbo <dg21stew@gmail.com>
2023-11-02 21:10:18 -0700

    Added more scanner examples(first and last name)

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,15 @@ public class Main {
         int number = scanner.nextInt();
         System.out.println(number + " is the number you entered");
 
+        System.out.println();
+
+        System.out.println("Enter first name: ");
+        String firstName = scanner.next();
+        System.out.println("Enter last name: ");
+        String lastName = scanner.next();
+
+        System.out.println("The name you entered is: " + firstName + " " + lastName);
+
     }
 
 }
